15c2387e574725cb2266555b5db3d30a645b37d1
dslackw <d.zlatanidis@gmail.com>
2014-05-07 22:25:26 +0300

    slpkg tool version 0.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='slpkg',
+    py_modules=['slpkg'],
+    version='0.0.1',
+    description="Python tool to manage Slackware packages",
+    keywords=["slpkg", "upgrade", "install", "remove", "view", "slackpkg", "tool"],
+    author="dslackw",
+    author_email="d.zlatanidis@gmail.com",
+    url="https://github.com/dslackw/slpkg",
+    download_url="https://github.com/dslackw/0.0.1.tar.gz",
+    scripts=['bin/slpkg'],
+    package_data={"": ["LICENSE", "README.rst", "CHANGELOG"]},
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 0 - Alpha",
+        "Topic :: Internet :: Utilities"],
+    long_description=open("README.rst").read()
+)
