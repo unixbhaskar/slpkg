520fbdd484ac3aa651d428f312e4a6854d5ee631
dslackw <d.zlatanidis@gmail.com>
2014-05-09 09:27:55 +0300

    man page install support

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os, sys, shutil
+
 from distutils.core import setup
 
 setup(
     name='slpkg',
     py_modules=['slpkg'],
-    version='0.0.3',
+    version='1.0.1,
     description="Python tool to manage Slackware packages",
     keywords=["slpkg", "upgrade", "install", "remove", "view", "slackpkg", "tool"],
     author="dslackw",
     author_email="d.zlatanidis@gmail.com",
     url="https://github.com/dslackw/slpkg",
-    download_url="https://github.com/dslackw/slpkg/archive/v0.0.3.tar.gz",
+    download_url="https://github.com/dslackw/slpkg/archive/v1.0.1.tar.gz",
     scripts=['bin/slpkg'],
     package_data={"": ["LICENSE", "README.rst", "CHANGELOG"]},
     classifiers=[
@@ -21,6 +23,15 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Development Status :: 0 - Alpha",
-        "Topic :: Internet :: Utilities"],
+        "Topic :: Intersetup.pynet :: Utilities"],
     long_description=open("README.rst").read()
 )
+
+if 'install' in sys.argv:
+    man_path = "/tmp/SBo/package-slpkg/usr/man/man8/"
+    os.system("mkdir -p {}".format(man_path))
+    if os.path.exists(man_path):
+        print("Installing man pages")
+        man_page = "man/slpkg.8"
+        shutil.copy2(man_page, man_path)
+        os.chmod(man_path + 'slpkg.8', int('444', 8))
