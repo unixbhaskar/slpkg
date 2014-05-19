58618e7ca978c99d633e16afbe8a6db842c350e5
dslackw <d.zlatanidis@gmail.com>
2014-05-19 12:25:26 +0300

    updated for version 1.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name = 'slpkg',
-    version = "1.2.2",
+    version = "1.2.3",
     description = "Python tool to manage Slackware packages",
     keywords = ["slackware", "slpkg", "upgrade", "install", "remove",
 		 "view", "slackpkg", "tool"],
