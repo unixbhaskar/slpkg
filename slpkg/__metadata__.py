d94c8d430172a100c0c71f8c14f4d6e98483d11d
dslackw <d.zlatanidis@gmail.com>
2014-07-21 02:11:55 +0300

    fix version

--- slpkg/__metadata__.py
+++ slpkg/__metadata__.py
@@ -4,7 +4,7 @@
 import os
 
 __author__ = "dslackw"
-__version_info__ = (1, 5, 9)
+__version_info__ = (1, 6, 0)
 __version__ = "{0}.{1}.{2}".format(*__version_info__)
 __license__ = "GNU General Public License v3 (GPLv3)"
 __email__ = "d.zlatanidis@gmail.com"
