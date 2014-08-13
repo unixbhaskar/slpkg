830b17167db60c988cf566c604962069bbb0b9ae
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2014-08-13 07:28:04 +0300

    updated license

--- slpkg/pkg/manager.py
+++ slpkg/pkg/manager.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# manager.py
+
+# Copyright 2014 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+# All rights reserved.
+
+# Utility for easy management packages in Slackware
+
+# https://github.com/dslackw/slpkg
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 import getpass
--- slpkg/sbo/dependency.py
+++ slpkg/sbo/dependency.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# dependency.py
+
+# Copyright 2014 Dimitris Zlatanidis <d.zlatanidis@gmail.com>
+# All rights reserved.
+
+# Utility for easy management packages in Slackware
+
+# https://github.com/dslackw/slpkg
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 
 from slpkg.colors import colors
@@ -67,7 +87,6 @@ def pkg_tracking(name):
         print ("| Package {0}{1}{2} dependencies :".format(colors.CYAN, name,
                                                            colors.ENDC))
         template(pkg_len)
-        #clear_dependencies.reverse()
         print ("\\")
         print (" +---{0}[ Tree of dependencies ]{1}".format(colors.YELLOW, colors.ENDC))
         index = 0
