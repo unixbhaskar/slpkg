271125487ad463b1be37ace80e00130ca02206d5
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2014-08-30 04:17:11 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Copyright 2014 Dimitris Zlatanidis Greece-Orestiada
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+PRGNAM=slpkg
+VERSION=${VERSION:-1.7.8}
+TAG=${TAG:-_dsw}
+
+cd ..
+# select archive to copy in slackbuild directory
+if [ -f $PRGNAM-$VERSION.zip ]; then
+    cp $PRGNAM-$VERSION.zip $PRGNAM-$VERSION/slackbuild
+    cd $PRGNAM-$VERSION/slackbuild
+    chmod +x $PRGNAM.SlackBuild
+    ./$PRGNAM.SlackBuild
+else
+    cp $PRGNAM-$VERSION.tar.gz $PRGNAM-$VERSION/slackbuild
+    cd $PRGNAM-$VERSION/slackbuild
+    chmod +x $PRGNAM.SlackBuild
+    ./$PRGNAM.SlackBuild
+fi
+# install or upgrade with new version
+upgradepkg --install-new /tmp/$PRGNAM-$VERSION-*$TAG.tgz
